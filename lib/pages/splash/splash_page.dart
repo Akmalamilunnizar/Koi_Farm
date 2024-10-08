3e1b1e6f82db62e1afed3d521d3b0e3572aebdb5
Nomikuza <uzumakisujai@gmail.com>
2024-10-08 18:20:17 +0700

    refactor: changing signup page front end

--- lib/pages/splash/splash_page.dart
+++ lib/pages/splash/splash_page.dart
@@ -2,11 +2,13 @@ import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:koi_farm/controllers/user_controller.dart';
 import 'package:koi_farm/routes/route_helper.dart';
+import 'package:koi_farm/utils/colors.dart';
+import 'package:koi_farm/utils/dimensions.dart';
 
 class SplashScreen extends StatefulWidget {
-
   const SplashScreen({super.key});
 
   @override
@@ -28,7 +30,7 @@ class _SplashScreenState extends State<SplashScreen>
   void initState() {
     super.initState();
     _loadResource();
-    
+
     // WidgetsBinding.instance.addPostFrameCallback((_) {
     //   ApiChecker();
     // });
@@ -43,23 +45,44 @@ class _SplashScreenState extends State<SplashScreen>
 
 //1:47:25
   @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-              child: Image.asset(
-            "assets/image/logo_jember.png",
-            width: 250,
-          )),
-          Center(
-              child: Image.asset(
-            "assets/image/scuderia_ferrari.png",
-            width: 250,
-          ))
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Koi Farm",
+              style: GoogleFonts.passeroOne(
+                  fontSize: 62,color: AppColors.splashTextColor),
+            ),
+            Image.asset(
+              "assets/image/koi_splash.jpg",
+              width: 250,
+            ),
+            Text(
+              "Selamat Datang",
+              style: TextStyle(
+                  fontSize: Dimensions.font32,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.splashTextColor),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                "Nikmati kemudahan dalam memantau kondisi kolam dan kesehatan ikan koi Anda secara real-time.",
+                style: GoogleFonts.poppins(
+                    fontSize: Dimensions.font12,
+                    fontWeight: FontWeight.w400,
+                    color: AppColors.splashTextColor),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
