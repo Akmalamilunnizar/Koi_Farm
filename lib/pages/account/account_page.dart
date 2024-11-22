5a39c70e9c2b1197d6cc1f24549491f2749f568a
Nomikuza <uzumakisujai@gmail.com>
2024-11-22 21:50:21 +0700

    fix (accountpage) : infinite loading

--- lib/pages/account/account_page.dart
+++ lib/pages/account/account_page.dart
@@ -23,173 +23,121 @@ class AccountPage extends StatelessWidget {
       Get.find<UserController>().getUserInfo();
       print("User telah login");
     }
+
     return Scaffold(
       appBar: CustomAppBar(title: "Profil"),
       body: GetBuilder<UserController>(builder: (userController) {
-        return _userLoggedIn
-            ? (userController.isLoading
-                ? Container(
-                    width: double.maxFinite,
-                    margin: EdgeInsets.only(top: Dimensions.height20),
-                    child: Column(
-                      children: [
-                        //profile icon
-                        AppIcon(
-                          icon: Icons.person_outline_rounded,
-                          backgroundColor: AppColors.mainColor,
-                          iconColor: Colors.white,
-                          iconSize: Dimensions.height45 + Dimensions.height30,
-                          size: Dimensions.height15 * 10,
-                        ),
-                        SizedBox(
-                          height: Dimensions.height30,
-                        ),
-                        Expanded(
-                          child: SingleChildScrollView(
-                            child: Column(
-                              children: [
-                                // name
-                                AccountWidget(
-                                    appIcon: AppIcon(
-                                      icon: Icons.person_outline_rounded,
-                                      backgroundColor: AppColors.mainColor,
-                                      iconColor: Colors.white,
-                                      iconSize: Dimensions.height10 * 5 / 2,
-                                      size: Dimensions.height10 * 5,
-                                    ),
-                                    bigText: BigText(
-                                        text: userController.userModel!.name)),
-                                SizedBox(
-                                  height: Dimensions.height20,
-                                ),
-                                // phone
-                                AccountWidget(
-                                    appIcon: AppIcon(
-                                      icon: Icons.phone,
-                                      backgroundColor: AppColors.yellowColor,
-                                      iconColor: Colors.white,
-                                      iconSize: Dimensions.height10 * 5 / 2,
-                                      size: Dimensions.height10 * 5,
-                                    ),
-                                    bigText: BigText(
-                                        text: userController.userModel!.phone)),
-                                SizedBox(
-                                  height: Dimensions.height20,
-                                ),
-                                // email
-                                AccountWidget(
-                                    appIcon: AppIcon(
-                                      icon: Icons.email_rounded,
-                                      backgroundColor: AppColors.yellowColor,
-                                      iconColor: Colors.white,
-                                      iconSize: Dimensions.height10 * 5 / 2,
-                                      size: Dimensions.height10 * 5,
-                                    ),
-                                    bigText: BigText(
-                                        text: userController.userModel!.email)),
-                                SizedBox(
-                                  height: Dimensions.height20,
-                                ),
-                                // // address
-                                // AccountWidget(
-                                //     appIcon: AppIcon(
-                                //       icon: Icons.location_on_rounded,
-                                //       backgroundColor: AppColors.yellowColor,
-                                //       iconColor: Colors.white,
-                                //       iconSize: Dimensions.height10 * 5 / 2,
-                                //       size: Dimensions.height10 * 5,
-                                //     ),
-                                //     bigText: BigText(text: "Isi alamat rumah")),
-                                // SizedBox(
-                                //   height: Dimensions.height20,
-                                // ),
-                                // message
-                                AccountWidget(
-                                    appIcon: AppIcon(
-                                      icon: Icons.message_rounded,
-                                      backgroundColor: Colors.redAccent,
-                                      iconColor: Colors.white,
-                                      iconSize: Dimensions.height10 * 5 / 2,
-                                      size: Dimensions.height10 * 5,
-                                    ),
-                                    bigText: BigText(text: "Pesan")),
-                                SizedBox(
-                                  height: Dimensions.height20,
-                                ),
-                                GestureDetector(
-                                  onTap: () {
-                                    if (Get.find<AuthController>()
-                                        .userLoggedIn()) {
-                                      Get.find<AuthController>()
-                                          .clearSharedData();                                      
-                                      Get.offNamed(RouteHelper.getSignInPage());
-                                    } else {
-                                      print("you logged out");
-                                    }
-                                  },
-                                  child: AccountWidget(
-                                      appIcon: AppIcon(
-                                        icon: Icons.logout_rounded,
-                                        backgroundColor: Colors.redAccent,
-                                        iconColor: Colors.white,
-                                        iconSize: Dimensions.height10 * 5 / 2,
-                                        size: Dimensions.height10 * 5,
-                                      ),
-                                      bigText: BigText(text: "Logout")),
-                                ),
-                                SizedBox(
-                                  height: Dimensions.height20,
-                                ),
-                              ],
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  )
-                : CustomLoader())
+        return userController.isLoading
+            ? CustomLoader()
             : Container(
-                child: Center(
-                    child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: double.maxFinite,
-                    height: Dimensions.height20 * 8,
-                    margin: EdgeInsets.only(
-                        left: Dimensions.width20, right: Dimensions.width20),
-                    decoration: BoxDecoration(
-                        borderRadius:
-                            BorderRadius.circular(Dimensions.radius20),
-                        image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: AssetImage(
-                                "assets/image/signintocontinue.png"))),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Get.toNamed(RouteHelper.getSignInPage());
-                    },
-                    child: Container(
-                      width: double.maxFinite,
-                      height: Dimensions.height20 * 4,
-                      margin: EdgeInsets.only(
-                          left: Dimensions.width20, right: Dimensions.width20),
-                      decoration: BoxDecoration(
-                        color: AppColors.mainColor,
-                        borderRadius:
-                            BorderRadius.circular(Dimensions.radius20),
-                      ),
-                      child: Center(
-                          child: BigText(
-                        text: "Masuk",
-                        color: Colors.white,
-                        size: Dimensions.font26,
-                      )),
+                width: double.maxFinite,
+                margin: EdgeInsets.only(top: Dimensions.height20),
+                child: Column(
+                  children: [
+                    // Profile icon
+                    AppIcon(
+                      icon: Icons.person_outline_rounded,
+                      backgroundColor: AppColors.mainColor,
+                      iconColor: Colors.white,
+                      iconSize: Dimensions.height45 + Dimensions.height30,
+                      size: Dimensions.height15 * 10,
                     ),
-                  )
-                ],
-              )));
+                    SizedBox(
+                      height: Dimensions.height30,
+                    ),
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            // Name
+                            AccountWidget(
+                              appIcon: AppIcon(
+                                icon: Icons.person_outline_rounded,
+                                backgroundColor: AppColors.mainColor,
+                                iconColor: Colors.white,
+                                iconSize: Dimensions.height10 * 5 / 2,
+                                size: Dimensions.height10 * 5,
+                              ),
+                              bigText:
+                                  BigText(text: userController.userModel!.name),
+                            ),
+                            SizedBox(
+                              height: Dimensions.height20,
+                            ),
+                            // Phone
+                            AccountWidget(
+                              appIcon: AppIcon(
+                                icon: Icons.phone,
+                                backgroundColor: AppColors.yellowColor,
+                                iconColor: Colors.white,
+                                iconSize: Dimensions.height10 * 5 / 2,
+                                size: Dimensions.height10 * 5,
+                              ),
+                              bigText: BigText(
+                                  text: userController.userModel!.phone),
+                            ),
+                            SizedBox(
+                              height: Dimensions.height20,
+                            ),
+                            // Email
+                            AccountWidget(
+                              appIcon: AppIcon(
+                                icon: Icons.email_rounded,
+                                backgroundColor: AppColors.yellowColor,
+                                iconColor: Colors.white,
+                                iconSize: Dimensions.height10 * 5 / 2,
+                                size: Dimensions.height10 * 5,
+                              ),
+                              bigText: BigText(
+                                  text: userController.userModel!.email),
+                            ),
+                            SizedBox(
+                              height: Dimensions.height20,
+                            ),
+                            // Message
+                            AccountWidget(
+                              appIcon: AppIcon(
+                                icon: Icons.message_rounded,
+                                backgroundColor: Colors.redAccent,
+                                iconColor: Colors.white,
+                                iconSize: Dimensions.height10 * 5 / 2,
+                                size: Dimensions.height10 * 5,
+                              ),
+                              bigText: BigText(text: "Pesan"),
+                            ),
+                            SizedBox(
+                              height: Dimensions.height20,
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                if (Get.find<AuthController>().userLoggedIn()) {
+                                  Get.find<AuthController>().clearSharedData();
+                                  Get.offNamed(RouteHelper.getSignInPage());
+                                } else {
+                                  print("You are already logged out.");
+                                }
+                              },
+                              child: AccountWidget(
+                                appIcon: AppIcon(
+                                  icon: Icons.logout_rounded,
+                                  backgroundColor: Colors.redAccent,
+                                  iconColor: Colors.white,
+                                  iconSize: Dimensions.height10 * 5 / 2,
+                                  size: Dimensions.height10 * 5,
+                                ),
+                                bigText: BigText(text: "Logout"),
+                              ),
+                            ),
+                            SizedBox(
+                              height: Dimensions.height20,
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ); // Show loading if `isLoading` is true
       }),
     );
   }
